4d9d095a2dc976a1c7c25f3ef33300beb7fc3db7
Matt Copperwaite <matt@copperwaite.net>
2020-08-26 16:59:30 +0100

    tests and refactor for main
    Bit of refactoring needed to make running tests possible without mocks

---
+++ tests/test_main.py
@@ -0,0 +1,12 @@
+import unittest
+
+from __main__ import create_args
+
+class TestGetArgs(unittest.TestCase):
+    def test_use_json(self):
+        args = create_args().parse_args("--json".split(" "))
+        self.assertTrue(args.use_json_output)
+
+    def test_not_use_json(self):
+        args = create_args().parse_args("".split(" "))
+        self.assertFalse(args.use_json_output)
--- textstat_cli/__main__.py
+++ textstat_cli/__main__.py
@@ -5,32 +5,38 @@ import textstat
 
 from .cli import TextStatCli
 
-def get_args():
+def create_args():
+    """
+    Creates arguments for this main function.
+    Returns the created parser object.
+    """
     parser = argparse.ArgumentParser(description='Get stats about text files')
     parser.add_argument('--language', '-l',
         default="en_US",
         help='Language to use as defined by en_US.'
     )
-    parser.add_argument('--json', '-j', action='store_false',
+    parser.add_argument('--json', '-j', action='store_true', default=False,
+        dest="use_json_output",
         help='Use argument to have the results output as json.'
     )
     parser.add_argument('path', help='Where to find these files to parse.')
+    return parser
 
-    return parser.parse_args()
-
-
-if __name__ == "__main__":
-    args = get_args()
-
-    textstat_cli = TextStatCli.from_args(args)
-    result = dict(textstat_cli)
-    if args.json:
-        print(json_dumps(result))
+def render_output(textstat_cli, args):
+    if args.use_json_output:
+        print(json_dumps(dict(textstat_cli)))
     else:
-        for file_name in result:
+        text = ""
+        for file_name in dict(textstat_cli):
             print(file_name)
             for test in result[file_name]:
                 print("\t{test}: {score}".format(
                     test=test,
                     score=result[file_name][test]
                 ))
+
+
+if __name__ == "__main__":
+    args = create_args().parse_args()
+    textstat_cli = TextStatCli.from_args(args)
+    render_output(textstat_cli, args)
