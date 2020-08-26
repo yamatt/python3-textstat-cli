80772906400a161239103deaeaa489b0377729f8
Matt Copperwaite <matt@copperwaite.net>
2020-08-26 17:01:28 +0100

    adds stub for file walk test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,16 +3,5 @@ import unittest
 from textstat_cli.cli import TextStat
 
 class TestTextStatCLI(unittest.TestCase):
-    def setUp(self):
-        pass
-
-
-class TestTextStatFile(unittest.TestCase):
-    def setUp(self):
-        pass
-
-
-class TestWalkPath(unittest.TestCase):
-    def setUp(self):
-        # create directory
+    def test_file_walk(self):
         pass
