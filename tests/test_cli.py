3f4b13cbb552e5c3bb285b8cdfbe3ab554c2c266
Matt Copperwaite <matt@copperwaite.net>
2020-08-28 21:27:03 +0100

    WIP tests for files.py and cli.py
    Missing tests for walk and dict in cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,30 @@
 import unittest
 
-from textstat_cli.cli import TextStat
+from textstat import textstat
+
+from textstat_cli.cli import TextStatCli
+
 
 class TestTextStatCLI(unittest.TestCase):
+    def test_textstat_singleton(self):
+        test_textstat = TextStatCli(None)
+        self.assertNone(test_textstat._textstat)
+        result = test_textstat.textstat
+        self.assertIsNotNone(test_textstat._textstat)
+        self.assertEqual(id(result), id(test_textstat.textstat))
+
+    def test_tests_exist(self):
+        for test_name in TextStatCli.TESTS:
+            self.assertTrue(hasattr(textstat, test))
+
+    def test_from_args_cls(self):
+        class MockArgs:
+            path = "test path"
+            language = "test language"
+
+        result = TextStatCli.from_args(MockArgs)
+        self.assertEqual(result.root_path, MockArgs.path)
+        self.assertEqual(result.language, MockArgs.language)
+
     def test_file_walk(self):
         pass
--- tests/test_files.py
+++ tests/test_files.py
@@ -1,29 +1,63 @@
+from io import StringIO
+from tempfile import NamedTemporaryFile
 import unittest
 from unittest.mock import Mock
 
 from textstat_cli.files import TextStatFile
 
-class TestTextStatFile(unittest.TestCase):
 
-    def test_getattr(self):
-        class mock_textstatcli:
-            class textstat:
-                mock_attr = "test result"
-        textstatfile = TextStatFile(None, mock_textstatcli)
-        self.assertEqual(textstatfile.textstat, mock_textstatcli.textstat.mock_attr)
-        self.assertRaises(AttributeError, textstatfile.fake_attribute)
+class TestTextStatFile(unittest.TestCase):
+    def test_getattr_calls_self(self):
+        class TestTextStatFile(TextStatFile):
+            test_value = "test value"
+
+        test_textstatfile = TextStatFile(None, None)
+
+        self.assertEqual(test_textstatfile.test_value, TestTextStatFile.test_value)
+
+    def test_getattr_calls_textstat(self):
+        class MockTextStatCLI:
+            textstat = Mock()
+
+        test_method_name = "test_method_name"
+
+        test_textstatfile = TextStatFile(None, MockTextStatCLI)
+
+        getattr(test_textstatfile, test_method_name)()
+
+        getattr(MockTextStatCLI.textstat, test_method_name).assert_called()
+
+    def test_text(self):
+        test_file_contents = "test file contents"
+        test_file = StringIO(test_file_contents)
+        test_textstatfile = TextStatFile(test_file, None)
+
+        self.assertIsNone(test_textstatfile._text)
+        test_text_result = test_textstatfile.text
+        self.assertEqual(test_textstatfile.text, test_file_contents)
+        self.assertEqual(id(test_textstatfile.text), id(test_text_result))
+        self.assertEqual(test_textstatfile._text, test_file_contents)
 
     def test_to_dict(self):
-        class mock_textstatcli:
-            TESTS = [
-                "mock1",
-                "mock2"
-            ]
+        test_method_name = "test1"
+        test_method_result = "result1"
 
-            def mock1(self):
-                return "mock1"
+        class MockTextStatCLI:
+            TESTS = [test_method_name]
 
-            def mock2(self):
-                return "mock2"
+            textstat = Mock(return_value=test_method_result)
 
-        textstatfile = TextStatFile(None, mock_textstatcli)
+        test_textstatfile = TextStatFile(None, MockTextStatCLI)
+
+        result = dict(test_textstatfile)
+
+        self.assertTrue(test_method_name in result)
+        self.assertEqual(result[test_method_name], test_method_result)
+
+    def test_from_path_cls(self):
+        mock_textstatcli = Mock()
+        temp_file = NamedTemporaryFile()
+
+        test_textstatfile = TextStatFile.from_path(temp_file.name, mock_textstatcli)
+        self.assertIsNotNone(test_textstatfile.f)
+        self.assertEqual(test_textstatfile.cli, mock_textstatcli)
